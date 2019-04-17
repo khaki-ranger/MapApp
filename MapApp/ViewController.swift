c147234041f38dc695d83231c6ed63121e182e86
Terashima <yamanogomora@gmail.com>
2019-04-18 08:52:57 +0900

    Add: 検索窓に入力したキーワードから緯度経度を取得して、マップ上にピンを配置する処理を実装

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if let searchKey = textField.text {
+            print(searchKey)
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+                if let unwrapPlacemarks = placemarks {
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        if let location = firstPlacemark.location {
+                            let targetCoordinate = location.coordinate
+                            print(targetCoordinate)
+                            let pin = MKPointAnnotation()
+                            pin.coordinate = targetCoordinate
+                            pin.title = searchKey
+                            self.dispMap.addAnnotation(pin)
+                            var region = self.dispMap.region
+                            region.center = targetCoordinate
+                            region.span.latitudeDelta = 0.02
+                            region.span.longitudeDelta = 0.02
+                            self.dispMap.setRegion(region, animated: true)
+                        }
+                    }
+                }
+            })
+        }
+        
+        return true
+    }
+    
 }
 
